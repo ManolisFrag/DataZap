3f7417e57fc09bc54cae5b92b97a01b2d09a7b33
varg man <vargmanolis@gmail.com>
2016-04-20 12:35:51 +0200

    20-04 pause menu added, press Esc

---
+++ Assets/PauseMenuC.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuC : MonoBehaviour {
+
+    public bool isPaused;
+    public GameObject Canvas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (isPaused)
+        {
+            Canvas.SetActive(true);
+            Time.timeScale = 0f;
+        } else
+        {
+            Canvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            isPaused = !isPaused;
+        }
+	}
+
+    public void Resume()
+    {
+        isPaused = false;
+
+    }
+}
---
+++ Assets/PauseScreen.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var pauseCanvas : Canvas;
+function Start () {
+    Screen.lockCursor = true;
+    Cursor.visible = false;
+}
+
+function Update () 
+{
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+        //(gameObject.Find("Third Person Controller").GetComponent("MouseLook") as MonoBehaviour).enabled = false;
+        (gameObject.Find("Main Camera").GetComponent("MouseLook") as MonoBehaviour).enabled = false;
+        pauseCanvas.enabled = true;
+        Time.timeScale = 0;
+        Screen.lockCursor = false;
+        Cursor.visible = true;
+    }
+
+
+}
+
+function ResumeGame () {
+
+}
